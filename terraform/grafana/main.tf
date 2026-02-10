292d4424f85ffd0671e3eb04cd3368cad2c78308
Hadeed Ahmed <hadeedahmeda5@gmail.com>
2026-02-10 15:05:00 +0500

    Create dedicated VPC for Grafana instead of sharing with SonarQube

--- terraform/grafana/main.tf
+++ terraform/grafana/main.tf
@@ -1,20 +1,42 @@
 
 
-# Look up the SonarQube VPC by its unique CIDR block
-data "aws_vpc" "sonar_vpc" {
-  cidr_block = "10.0.0.0/16"
+# Create dedicated VPC for Grafana
+resource "aws_vpc" "grafana_vpc" {
+  cidr_block           = "10.1.0.0/16"
+  enable_dns_hostnames = true
+  tags = { Name = "grafana-vpc" }
 }
 
-# Look up the SonarQube subnet by CIDR and VPC
-data "aws_subnet" "sonar_subnet" {
-  vpc_id     = data.aws_vpc.sonar_vpc.id
-  cidr_block = "10.0.1.0/24"
+resource "aws_internet_gateway" "grafana_igw" {
+  vpc_id = aws_vpc.grafana_vpc.id
+  tags = { Name = "grafana-igw" }
+}
+
+resource "aws_subnet" "grafana_subnet" {
+  vpc_id                  = aws_vpc.grafana_vpc.id
+  cidr_block              = "10.1.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+  tags = { Name = "grafana-subnet" }
+}
+
+resource "aws_route_table" "grafana_rt" {
+  vpc_id = aws_vpc.grafana_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.grafana_igw.id
+  }
+}
+
+resource "aws_route_table_association" "grafana_rta" {
+  subnet_id      = aws_subnet.grafana_subnet.id
+  route_table_id = aws_route_table.grafana_rt.id
 }
 
 # Use the VPC and Subnet IDs
 locals {
-  vpc_id    = data.aws_vpc.sonar_vpc.id
-  subnet_id = data.aws_subnet.sonar_subnet.id
+  vpc_id    = aws_vpc.grafana_vpc.id
+  subnet_id = aws_subnet.grafana_subnet.id
 }
 
 resource "aws_security_group" "grafana_sg" {
